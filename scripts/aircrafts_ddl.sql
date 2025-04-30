2249a4b4158e27021321139d90cea78a27bf61c8
Серафим Ситников <sitnikov.sv@phystech.edu>
2025-04-30 18:49:00 +0300

    add files

--- scripts/aircrafts_ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE IF NOT EXISTS Aircrafts
-(
-    id      SERIAL PRIMARY KEY,
-    company varchar(20),
-    airline varchar(20),
-    model   varchar(20)
-);
--- scripts/airlines_ddl.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS Airlines
-(
-    airline_id      SERIAL PRIMARY KEY,
-    basic_country varchar(20),
-    planes_count varchar(20),
-    model   varchar(20)
-);
-
